ae77525a1278a2f95bd79d73c8c8e28b21be90a2
“keinet” <keisukeikegami15@gmail.com>
2025-06-29 23:05:41 +0900

    fix: 不要な処理の整理

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@
 // exit;
 
 error_reporting(E_ALL);
-ini_set("display_error", 1);
+ini_set("display_errors", 1);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['index'])) {
   $index = (int)$_POST['index'];
@@ -18,8 +18,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['index'])) {
 
   // 指定された行を削除
   if (isset($rows[$index])) {
+      // 念の為、削除対象のログをログ出力しておくと安心
+      error_log("削除: " . implode(",", $rows[$index]));
+
       unset($rows[$index]);
       $rows = array_values($rows); // 添え字をリセット（←これがないと行がズレる）0,1,2 1なし　0,2 これを直す
+  } else {
+      error_log("指定インデックス $index が存在しません");
+  }
+
+  while ((&data = fgetcsv($file)) !== false) {
+      if (!empty(array_filter($data))) {
+          $rows[] = $data;
+      }
   }
 
   // 書き戻し
